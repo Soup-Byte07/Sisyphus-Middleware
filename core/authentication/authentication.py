109610b264d4382acfadc8813d51027c6fa1d8d5
Soup-Byte07 <soupbyte30@gmail.com>
2025-05-21 03:39:24 -0400

    lol idk, lmao

--- core/authentication/authentication.py
+++ core/authentication/authentication.py
@@ -11,15 +11,25 @@ class AuthenticationHandler(BaseModel):
     def authenticate(self, request: Request) -> Response:
         raise NotImplementedError("Subclasses should implement this method.")
 
-class BasicAuthenticationHandler():
-    auth_handler: AuthenticationHandler
 
-    def __init__(self, username: str, password: str, mod_name: str = ""):
+class RegisterLibAuthenticationHandler():
+    auth_handler: AuthenticationHandler
+    def __init__(self, username: str, password: str, mod_name: str = "", register_name: str = ""):
         self.mod_name = mod_name
         self.auth_handler = AuthenticationHandler(username=username, password=SecretStr(password))
+        register_mod_lib(mod_name, register_name)
 
-        register_mod_lib(mod_name, "BasicAuthenticationHandler")
 
+
+class BasicAuthenticationHandler(RegisterLibAuthenticationHandler):
+    register_name: str = "BasicAuthenticationHandler"
     def create_auth_header(self) -> BasicAuth:
         return BasicAuth(self.auth_handler.username, self.auth_handler.password.get_secret_value())
+
+
+class BearerAuthenticationHandler(RegisterLibAuthenticationHandler):
+    register_name: str = "BasicAuthenticationHandler"
+    def create_auth_header(self, token: str) -> str:
+        return token
+
 
