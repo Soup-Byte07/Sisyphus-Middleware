2ade43692d015e461709fbe24b3566638bd784cf
Soup-Byte07 <soupbyte30@gmail.com>
2025-05-23 05:42:18 -0400

    Cors Support

--- core/sisyphus.py
+++ core/sisyphus.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, APIRouter
 from core.scripts.loader import load_toml_config
 from core.logging.logging import invalid_port
@@ -10,8 +11,22 @@ class Sisyphus:
     def __init__(self):
         self.app: FastAPI = FastAPI()
         self.ssl_config = None
+        self.cors_config = None
         self.config = load_toml_config("sisyphus.toml")
         self.port: int = invalid_port(int(self.config["port"]))
+        
+
+
+        if self.config["cors"]["use_cors"] == True:
+            self.cors_config = self.config["cors"]
+            self.app.add_middleware(
+                CORSMiddleware,
+                allow_origins=self.cors_config["allow_origin"],
+                allow_credentials=self.cors_config["allow_credentials"],
+                allow_methods=self.cors_config["allow_methods"],
+                allow_headers=self.cors_config["allow_headers"],
+            )
+
         
         if self.config["load_cert"]["load"] == True:
             self.ssl_config = SSLCertificateManager(self.config["load_cert"])
