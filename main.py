efed53f74bc819b9a1446de5ae87a4f302e87cab
Soup-Byte07 <soupbyte30@gmail.com>
2025-06-24 19:02:04 -0400

    uhh small things

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from core.sisyphus import Sisyphus
 
 from mods.example_pxy.example_pxy import ExampleMod
+from mods.hof.hof import Hof
 
 s = Sisyphus()
-_ExampleMod = ExampleMod(s)
+#_ExampleMod = ExampleMod(s)
+_Hof = Hof(s)
 
 s.run()
